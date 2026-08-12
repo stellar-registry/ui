f7d382307ae270079ac501d1f7c42ec5c369abee
Pam S <pam@theaha.co>
2026-08-12 15:44:44 -0400

    feat: Contract version history (#40)
    Display contract version history on contract detail page. Versions are
    displayed in reverse chronological order (latest on top).
    
    <img width="622" height="142" alt="contract-history2"
    src="https://github.com/user-attachments/assets/a3ee1073-f639-473d-9d18-476954050d02"
    />

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -99,11 +99,20 @@ export interface Contract {
 	is_stellar_asset_contract: boolean
 }
 
+interface ContractVersion {
+	wasm_name: string
+	wasm_version: string
+	wasm_channel?: string
+	version_index: number
+	kind: string
+}
+
 export interface ContractDetail extends Contract {
 	id: string
 	transaction_hash: string
 	ledger_sequence: number
 	created_at: string
+	versions: ContractVersion[]
 }
 
 export interface SearchParams {
